306481fb9c7d5aa0213d58b34bc2b4b1b61350d8
Lachlan Sleight <lachlansleight@gmail.com>
2020-10-30 20:56:05 +1100

    temp: Add support for UnityXR (doesn't work with Index :c)

---
+++ Assets/_Lineagyra/Scripts/VrRaycaster.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR;
+
+public class VrRaycaster : MonoBehaviour
+{
+    public XRNode Device = XRNode.RightHand;
+    private InputDevice _device;
+    
+    private void GetDevice()
+    {
+        var devices = new List<InputDevice>();
+        InputDevices.GetDevices(devices);
+        foreach (var device in devices) {
+            Debug.Log(device.name + " - " + device.characteristics.ToString());
+        }
+        
+        _device = InputDevices.GetDeviceAtXRNode(Device);
+        var usages = new List<InputFeatureUsage>();
+        _device.TryGetFeatureUsages(usages);
+        foreach (var usage in usages) {
+            Debug.Log(usage.name + "\t" + usage.type);
+        }
+    }
+
+    void Update()
+    {
+        if (!_device.isValid) {
+            GetDevice();
+            return;
+        }
+        
+        CurvedUIInputModule.CustomControllerRay = new Ray(transform.position, transform.forward);
+        var success = _device.TryGetFeatureValue(CommonUsages.triggerButton, out var triggerPressed);
+        CurvedUIInputModule.CustomControllerButtonState = triggerPressed;
+
+        Debug.Log($"{success} + {triggerPressed} + {Input.GetButton("RightTrigger")}");
+    }
+}
